43378e3943d791513c2e920bb101976a6b0c60c8
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-03-30 16:18:37 +0530

    save

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="hero">
+      <div className="flex-1 padding-x pt-36">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;

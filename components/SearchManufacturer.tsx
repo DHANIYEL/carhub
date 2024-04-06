82ad1aa7eb8fc55c072bb69c435f5e0a9bc2553a
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-04-07 01:39:17 +0530

    manufacturer still

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,36 @@
 import { SearchManufacturerProps } from "@/types";
-import React from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  const [query, setQuery] = useState("");
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button>
+            <Image
+              src="./car-logo.svg"
+              alt="car logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer-input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          ></Combobox.Input>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;

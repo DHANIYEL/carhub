84a30f4a8ff1f12a6c0f081eca2b0bdc3de4b6b5
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-07-20 14:46:42 +0530

    card mpg

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -7,12 +7,16 @@ import React from "react";
 import { calculateCarRent } from "@/utils";
 import Image from "next/image";
 
-const CarCard = ({ car }: CarProps) => {
+interface carCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: carCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
   const carRent = calculateCarRent(city_mpg, year);
   return (
-    <div className="car-card group">
+    <div className="car-card group my-6">
       <div className="car-card__content">
         <h2 className="car-card__content-title">
           {make} {model}
@@ -31,6 +35,28 @@ const CarCard = ({ car }: CarProps) => {
           className="object-contain"
           alt="car img"
         />
+      </div>
+      <div className="relative w-full mt-2 flex">
+        <div className="flex group-hover:invisible justify-between w-full text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            {transmission === "a" ? "Automatic" : "Manual"}
+            <p className="text-[14px]"></p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tier" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="steering wheel" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
       </div>
     </div>
   );
